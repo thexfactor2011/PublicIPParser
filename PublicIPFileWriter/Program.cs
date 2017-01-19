88de82df8501167b38130c6a0eee1455c2a47e16
Harry Anuszewski <thexfactorlabs@gmail.com>
2017-01-19 08:44:32 -0500

    Initial Commit

---
+++ PublicIPFileWriter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+
+namespace PublicIPFileWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://whatismyip.org/";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string fileName = path + @"\Dropbox\personal\PublicIP.txt";
+
+            var web = new HtmlWeb();
+            var document = web.Load(url);
+            var page = document.DocumentNode;
+
+            foreach (var item in page.QuerySelectorAll("span"))
+            {
+                using (StreamWriter stream = new StreamWriter (fileName))
+                {
+                    stream.WriteLine(item.InnerText);
+                }
+                break;
+            }            
+        }        
+    }
+}
